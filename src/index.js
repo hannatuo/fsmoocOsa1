b63bfb02aa0bd38dbb5ab14c93ced044a62ec945
Hanna Tuomi <hannamtuomi@gmail.com>
2020-06-23 20:19:56 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+        {props.text}
+    </button>
+)
+
+const Display = props => <div>{props.name} {props.value}</div>
+
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button handleClick={() => setGood(good + 1)} text="good" />
+            <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+            <Button handleClick={() => setBad(bad + 1)} text="bad" />
+            <h1>Statistics</h1>
+            < Display name="Good" value={good}/>
+            <Display name="Neutral" value={neutral} />
+            <Display name="Bad" value={bad} />
+
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root')
+)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +43,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
